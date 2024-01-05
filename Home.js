9a433da97007bd4f5d412ad11c4475c61bbcba01
Tiodevs <Tiodevs>
2024-01-05 13:45:29 -0300

    first commit

---
+++ Home.js
@@ -0,0 +1,15 @@
+
+// Animação do menu versão mobi
+const btnMenu = document.getElementById('js-btn-menu');
+
+btnMenu.addEventListener('click', () => {
+    document.documentElement.classList.toggle('opened-menu')
+    btnMenu.classList.toggle('is-active')
+})
+
+// const conteudo = document.querySelector('.conteudo');
+
+// window.addEventListener('wheel', (event) => {
+//     window.scrollBy(0, event.deltaY * 5);
+//     event.preventDefault();
+//   });
